24bed4d59f321f6fbdca23f2416cd2b69c921934
James Tonnesen <james.tonnesen@nextraq.com>
2026-06-24 11:01:30 -0400

    chal-1 add POST todos and get by ID

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,28 +1,76 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/ErikTonnesen1/api-challenges/internal/dto"
 )
 
-func Todos(w http.ResponseWriter, r *http.Request) {
+// Use an interface as expected Handler param in order to inject test/mock services
+// Apparently, the definition of the Interface usually lives in the consumer, while the
+// concrete struct will live in the service package
+type TodoService interface {
+	GetAll() []dto.TodoItem
+	GetItem(id int) (dto.TodoItem, error)
+	AddItem(i dto.TodoItem) (dto.TodoItem, error)
+}
+
+type todoHandler struct {
+	service TodoService
+}
+
+func NewTodoHandler(s TodoService) *todoHandler {
+	return &todoHandler{
+		service: s,
+	}
+}
+
+func (h *todoHandler) Todos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	switch r.Method {
 	case http.MethodGet:
-		io.WriteString(w, "Hello from Get TODO's\n")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(h.service.GetAll())
 
 	case http.MethodPost:
-		io.WriteString(w, "Hello from POST TODO's\n")
+		var newItem dto.TodoItem
+		err := json.NewDecoder(r.Body).Decode(&newItem)
+		if err != nil {
+			http.Error(w, "invalid json", http.StatusBadRequest)
+			return
+		}
+		addedItem, err := h.service.AddItem(newItem)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(addedItem)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 
 	}
 }
 
-func TodosById(w http.ResponseWriter, r *http.Request) {
+func (h *todoHandler) TodosById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
 	case http.MethodGet:
-		io.WriteString(w, fmt.Sprintf("Hello from Get TODO's by ID for: %s\n", r.PathValue("id")))
+		requestedId := r.PathValue("id")
+		id, err := strconv.Atoi(requestedId)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Id must be of type int: %s", requestedId), http.StatusBadRequest)
+		}
+		todoItem, err := h.service.GetItem(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(todoItem)
+		}
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,16 +2,21 @@ package server
 
 import (
 	"fmt"
-	"github.com/ErikTonnesen1/api-challenges/internal/handler"
 	"log"
 	"net/http"
+
+	"github.com/ErikTonnesen1/api-challenges/internal/handler"
+	todoService "github.com/ErikTonnesen1/api-challenges/internal/service"
 )
 
 func StartServer() {
 	fmt.Println("Starting server...")
 
-	http.HandleFunc("/todos", handler.Todos)
-	http.HandleFunc("/todos/{id}", handler.TodosById)
+	todoService := todoService.NewTodoService()
+	todoHandler := handler.NewTodoHandler(todoService)
+
+	http.HandleFunc("/todos", todoHandler.Todos)
+	http.HandleFunc("/todos/{id}", todoHandler.TodosById)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
